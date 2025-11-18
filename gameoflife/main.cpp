eb5c72196efb571ada73beb352e06d5981d02bdb
AlCatSplat <83360294+AlCatSplat@users.noreply.github.com>
2025-11-17 17:55:35 -0800

    Added csv

--- gameoflife/main.cpp
+++ gameoflife/main.cpp
@@ -2,10 +2,22 @@
 * Conway's game of life in C++. Created by Alex Kibret, Raine Wong, and Andy Xiao.
 */
 #include <iostream>
+#include <fstream>
+
+void readFromFile();
 
 int main() {
 
 	std::cout << "Hello world.";
 
+	int cells[30][30];
+
 	return 0;
+}
+
+//TODO: Implement ability to read game state from CSV file
+void readFromFile() {
+
+	std::ifstream CellDataFile("cells.csv");
+
 }
