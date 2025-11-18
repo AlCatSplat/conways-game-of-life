1b9ea185971f5fd05bcd7b0388f6e88537d2a9a0
AlCatSplat <83360294+AlCatSplat@users.noreply.github.com>
2025-11-17 18:45:19 -0800

    Working on csv function

--- gameoflife/main.cpp
+++ gameoflife/main.cpp
@@ -3,21 +3,38 @@
 */
 #include <iostream>
 #include <fstream>
+#include <string>
 
-void readFromFile();
+void readFromFile(int cellArray, int xSize, int ySize);
 
 int main() {
 
-	std::cout << "Hello world.";
+	const int X_SIZE = 30;
+	const int Y_SIZE = 30;
 
-	int cells[30][30];
+	int cells[X_SIZE][Y_SIZE];
+
+	readFromFile(cells[X_SIZE][Y_SIZE], X_SIZE, Y_SIZE);
 
 	return 0;
 }
 
-//TODO: Implement ability to read game state from CSV file
-void readFromFile() {
+// TODO: Implement ability to read game state from CSV file
+void readFromFile(int cellArray, int xSize, int ySize) {
 
 	std::ifstream CellDataFile("cells.csv");
+	std::string line;
+	bool firstRow = true;
+
+	for (int i = 0; i < xSize; i++) {
+		//Work in progress
+	}
+
+	while (std::getline(CellDataFile, line)) {
+		if (firstRow) {
+			firstRow = false;
+			continue;
+		}
+	}
 
 }
